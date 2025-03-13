1be0d9b632668565621514b4efdfc5ff7de647e6
sahil <shp2382@g.rit.edu>
2025-03-13 16:56:09 -0400

    made apps and got simple view setup working

---
+++ backend/google_calendar/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+def calendar(request):
+    return HttpResponse("This is the calendar page.")
+# Create your views here.
---
+++ backend/notion/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+# Create your views here.
+
+def notion (request):
+    return HttpResponse("This is the notion page.")
